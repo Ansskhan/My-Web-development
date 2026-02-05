b77b6793fbb126734cdf8d9a5920ee323d4ba8a7
Ansskhan <Anas771khan@hotmail.com>
2026-02-05 23:53:27 +0500

    improvements

--- assets/js/components/fetch.js
+++ assets/js/components/fetch.js
@@ -17,10 +17,9 @@ document.addEventListener("DOMContentLoaded", function () {
 
         blogCard.innerHTML = `
           <div class="single-blog">
-            <div class="blog-thumb" loading="lazy" style="background-image: url('${card.image}')"></div>
+            <div class="blog-thumb" loading="auto" style="background-image: url('${card.image}')"></div>
             <h4 class="blog-title"><a href="single-blog.html">${card.title}</a></h4>
             <p>${card.description}</p>
-            <a href="single-blog.html" class="button">Read More</a>
           </div>
         `;
 
